47983e300fd4693b88fdcb7e335282d5ec86b6b7
au544079 <au544079@uni.au.dk>
2019-10-08 15:19:23 +0200

    fix

--- src/main/java/hotciv/standard/GameImpl.java
+++ src/main/java/hotciv/standard/GameImpl.java
@@ -175,7 +175,7 @@ public class GameImpl implements Game {
   public boolean moveUnit(Position from, Position to) {
     boolean isFromInTheWorld = world.containsKey(from);
     boolean isToInTheWorld = world.containsKey(to);
-    boolean isTileTypeAtToValidForMovement = world.get(to).isValidMovementTileType(units.get(from));
+    boolean isTileTypeAtToValidForMovement = isFromInTheWorld && isToInTheWorld && world.get(to).isValidMovementTileType(units.get(from));
     boolean isThereAUnitAtFrom = units.get(from) != null;
     boolean isThereAUnitAtTo = units.get(to) != null;
     boolean isUnitOwnedByPlayerInTurn = isThereAUnitAtFrom && units.get(from).getOwner() == playerInTurn;
