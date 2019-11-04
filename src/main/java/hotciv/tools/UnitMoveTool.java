489662f61b55ba59e23fc891d94d967825c46d7b
au544079 <au544079@uni.au.dk>
2019-11-04 16:50:23 +0100

    not much made

--- src/main/java/hotciv/tools/UnitMoveTool.java
+++ src/main/java/hotciv/tools/UnitMoveTool.java
@@ -2,13 +2,22 @@ package hotciv.tools;
 
 import hotciv.framework.Game;
 import minidraw.framework.DrawingEditor;
+import minidraw.framework.Figure;
+import minidraw.framework.Tool;
 import minidraw.standard.AbstractTool;
 import minidraw.standard.SelectionTool;
+import minidraw.standard.handlers.DragTracker;
 
-public class UnitMoveTool extends AbstractTool {
+public class UnitMoveTool extends AbstractTool implements Tool {
 
 
   private final Game game;
+
+  /**
+   * the figure that is being dragged. If null then its operation is not that of
+   * dragging a figure (or a set of figures)
+   */
+  protected Figure draggedFigure;
 
   public UnitMoveTool(DrawingEditor editor, Game game) {
     super(editor);
