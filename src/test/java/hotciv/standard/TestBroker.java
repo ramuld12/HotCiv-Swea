eac852faebe23f0b9209c644517ee19cd6000eb1
Rasmus Herskind <201808249@post.au.dk>
2019-11-19 11:33:28 +0100

    Started broker test

---
+++ src/test/java/hotciv/standard/TestBroker.java
@@ -0,0 +1,14 @@
+package hotciv.standard;
+
+import hotciv.framework.Game;
+import hotciv.stub.StubGame2;
+import org.junit.*;
+
+public class TestBroker {
+
+  @Before
+  public void setUp() {
+    Game servant = new StubGame2();
+
+  }
+}
