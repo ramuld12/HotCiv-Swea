af0d8a9c934b85e410829b7aa596c4037f9933f6
au544079 <au544079@uni.au.dk>
2019-11-19 13:14:03 +0100

    lala

--- src/main/java/hotciv/standard/Broker/HotCivGameInvoker.java
+++ src/main/java/hotciv/standard/Broker/HotCivGameInvoker.java
@@ -6,12 +6,17 @@ import hotciv.framework.Game;
 
 public class HotCivGameInvoker implements Invoker {
 
+  private Game servant;
+
   public HotCivGameInvoker(Game servant) {
+    this.servant = servant;
 
   }
 
   @Override
-  public ReplyObject handleRequest(String objectId, String operationName, String payload) {
-    return null;
+  public ReplyObject handleRequest(String objectId, String operationName, String payload)
+  {
+
+    return new ReplyObject(Integer.valueOf(objectId),operationName);
   }
 }
