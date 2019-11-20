7aa36e3eab1ce884be201f479957257ef2375d7a
au544079 <au544079@uni.au.dk>
2019-11-20 15:07:06 +0100

    gradle

--- src/main/java/hotciv/standard/Broker/GameLobby/HotCivClient.java
+++ src/main/java/hotciv/standard/Broker/GameLobby/HotCivClient.java
@@ -23,13 +23,10 @@ public class HotCivClient {
 
   public HotCivClient(String[] args) {
     parseCommandlineParameters(args);
-
-
     System.out.println("LobbyClient: Asked to do operation " + operation + " for player " + name);
     ClientRequestHandler clientRequestHandler
             = new SocketClientRequestHandler(hostName, BrokerConstants.serverPort);
     Requestor requestor = new StandardJSONRequestor(clientRequestHandler);
-
     Game lobby = new GameProxy(requestor);
   }
 
