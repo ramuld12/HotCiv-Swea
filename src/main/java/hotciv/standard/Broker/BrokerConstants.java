db0705a8fdd5d2564c57da14bec862b4f33e5e2d
au544079 <au544079@uni.au.dk>
2019-11-28 16:03:10 +0100

    suntring

--- src/main/java/hotciv/standard/Broker/BrokerConstants.java
+++ src/main/java/hotciv/standard/Broker/BrokerConstants.java
@@ -9,34 +9,34 @@ public class BrokerConstants {
   public static final String CITY_PREFIX = "City";
 
   //Methods for Game
-  public static final String getAgeString = GAME_PREFIX + "getAge";
-  public static final String getPlayerInTurnString = GAME_PREFIX + "getPlayerInTurn";
-  public static final String getWinnerString = GAME_PREFIX + "getWinner";
-  public static final String moveUnit_action = GAME_PREFIX + "moveUnit";
-  public static final String changeCityProduction = GAME_PREFIX + "changeProductionInCityAt";
-  public static final String endTurn_action = GAME_PREFIX + "endOfTurn";
-  public static final String unitAction = GAME_PREFIX + "performUnitActionAt";
-  public static final String GAME_GET_CITY_METHOD = GAME_PREFIX + "getCityAt";
-  public static final String GAME_GET_UNIT_METHOD = GAME_PREFIX + "getUnitAt";
-  public static final String GAME_GET_TILE_METHOD = GAME_PREFIX + "getTileAt";
+  public static final String getAgeString = GAME_PREFIX + "_getAge";
+  public static final String getPlayerInTurnString = GAME_PREFIX + "_getPlayerInTurn";
+  public static final String getWinnerString = GAME_PREFIX + "_getWinner";
+  public static final String moveUnit_action = GAME_PREFIX + "_moveUnit";
+  public static final String changeCityProduction = GAME_PREFIX + "_changeProductionInCityAt";
+  public static final String endTurn_action = GAME_PREFIX + "_endOfTurn";
+  public static final String unitAction = GAME_PREFIX + "_performUnitActionAt";
+  public static final String GAME_GET_CITY_METHOD = GAME_PREFIX + "_getCityAt";
+  public static final String GAME_GET_UNIT_METHOD = GAME_PREFIX + "_getUnitAt";
+  public static final String GAME_GET_TILE_METHOD = GAME_PREFIX + "_getTileAt";
 
   // Methods for City
-  public static final String getOwnerCity = CITY_PREFIX + "getOwner";
-  public static final String getSizeString = CITY_PREFIX + "getSize";
-  public static final String getTreasuryString = CITY_PREFIX + "getTreasury";
-  public static final String getProductionString = CITY_PREFIX + "getProduction";
-  public static final String getWorkforceFocus = CITY_PREFIX + "getWorkforceFocus";
+  public static final String getOwnerCity = CITY_PREFIX + "_getOwner";
+  public static final String getSizeString = CITY_PREFIX + "_getSize";
+  public static final String getTreasuryString = CITY_PREFIX + "_getTreasury";
+  public static final String getProductionString = CITY_PREFIX + "_getProduction";
+  public static final String getWorkforceFocus = CITY_PREFIX + "_getWorkforceFocus";
 
   //Methods for Tile
-  public static final String tileString = TILE_PREFIX + "getTileString";
+  public static final String tileString = TILE_PREFIX + "_getTileString";
 
   // Methods for unit
-  public static final String getOwnerUnit = UNIT_PREFIX + "getOwner";
-  public static final String getTypeString = UNIT_PREFIX + "getTypeString";
-  public static final String getMoveCountString = UNIT_PREFIX + "getMoveCount";
-  public static final String getDefensiveStrengthString = UNIT_PREFIX + "getDefensiveStrength";
-  public static final String getAttackingStrengthString = UNIT_PREFIX + "getAttackingStrength";
-  public static final String isUnitMoveable = UNIT_PREFIX + "isMoveable";
+  public static final String getOwnerUnit = UNIT_PREFIX + "_getOwner";
+  public static final String getTypeString = UNIT_PREFIX + "_getTypeString";
+  public static final String getMoveCountString = UNIT_PREFIX + "_getMoveCount";
+  public static final String getDefensiveStrengthString = UNIT_PREFIX + "_getDefensiveStrength";
+  public static final String getAttackingStrengthString = UNIT_PREFIX + "_getAttackingStrength";
+  public static final String isUnitMoveable = UNIT_PREFIX + "_isMoveable";
 
   //Server
   public static final int serverPort = 37123;
